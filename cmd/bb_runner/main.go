4bf225d84d84e0c1fddd78cb5589f7c89cb38d8c
Ed Schouten <eschouten@apple.com>
2019-12-14 15:11:49 -0800

    Remove support for default configuration values
    These values are hardly ever what is desired. Furthermore, they are
    often shared between applications, meaning that there is a reasonable
    risk that users may specify them for one application, but not the other,
    even though they need to remain in sync.
    
    If there is a true desire for default values, I think we should work
    towards providing a Jsonnet library that augments configuration files
    with defaults.

--- cmd/bb_runner/main.go
+++ cmd/bb_runner/main.go
@@ -6,9 +6,10 @@ import (
 	"os"
 
 	"github.com/buildbarn/bb-remote-execution/pkg/environment"
+	"github.com/buildbarn/bb-remote-execution/pkg/proto/configuration/bb_runner"
 	"github.com/buildbarn/bb-remote-execution/pkg/proto/runner"
-	"github.com/buildbarn/bb-remote-execution/pkg/configuration/bb_runner"
 	"github.com/buildbarn/bb-storage/pkg/filesystem"
+	"github.com/buildbarn/bb-storage/pkg/util"
 
 	"google.golang.org/grpc"
 )
@@ -17,24 +18,23 @@ func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("Usage: bb_runner bb_runner.jsonnet")
 	}
-
-	runnerConfiguration, err := configuration.GetRunnerConfiguration(os.Args[1])
-	if err != nil {
+	var configuration bb_runner.ApplicationConfiguration
+	if err := util.UnmarshalConfigurationFromFile(os.Args[1], &configuration); err != nil {
 		log.Fatalf("Failed to read configuration from %s: %s", os.Args[1], err)
 	}
 
-	buildDirectory, err := filesystem.NewLocalDirectory(runnerConfiguration.BuildDirectoryPath)
+	buildDirectory, err := filesystem.NewLocalDirectory(configuration.BuildDirectoryPath)
 	if err != nil {
 		log.Fatal("Failed to open build directory: ", err)
 	}
 
-	env := environment.NewLocalExecutionEnvironment(buildDirectory, runnerConfiguration.BuildDirectoryPath)
+	env := environment.NewLocalExecutionEnvironment(buildDirectory, configuration.BuildDirectoryPath)
 	var runnerServer runner.RunnerServer
 	// When temporary directories need cleaning prior to executing a build
 	// action, attach a series of TempDirectoryCleaningManagers.
-	if len(runnerConfiguration.TemporaryDirectories) > 0 {
+	if len(configuration.TemporaryDirectories) > 0 {
 		m := environment.NewSingletonManager(env)
-		for _, d := range runnerConfiguration.TemporaryDirectories {
+		for _, d := range configuration.TemporaryDirectories {
 			directory, err := filesystem.NewLocalDirectory(d)
 			if err != nil {
 				log.Fatalf("Failed to open temporary directory %#v: %s", d, err)
@@ -49,13 +49,13 @@ func main() {
 	s := grpc.NewServer()
 	runner.RegisterRunnerServer(s, runnerServer)
 
-	if err := os.Remove(runnerConfiguration.ListenPath); err != nil && !os.IsNotExist(err) {
-		log.Fatalf("Could not remove stale socket %#v: %s", runnerConfiguration.ListenPath, err)
+	if err := os.Remove(configuration.ListenPath); err != nil && !os.IsNotExist(err) {
+		log.Fatalf("Could not remove stale socket %#v: %s", configuration.ListenPath, err)
 	}
 
-	sock, err := net.Listen("unix", runnerConfiguration.ListenPath)
+	sock, err := net.Listen("unix", configuration.ListenPath)
 	if err != nil {
-		log.Fatalf("Failed to create listening socket %#v: %s", runnerConfiguration.ListenPath, err)
+		log.Fatalf("Failed to create listening socket %#v: %s", configuration.ListenPath, err)
 	}
 	if err := s.Serve(sock); err != nil {
 		log.Fatal("Failed to serve RPC server: ", err)
