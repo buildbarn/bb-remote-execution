10f4c1ddb5949b0069e987ec39261bf81213c771
Ed Schouten <eschouten@apple.com>
2021-11-11 10:24:59 +0100

    Process table cleaning: ignore EPERM
    We may observe these if there are setuid processes running. These can
    generally not be killed. I think the only sensible thing here is to just
    ignore these errors, as they are not actionable.

--- pkg/cleaner/process_table_cleaner_unix.go
+++ pkg/cleaner/process_table_cleaner_unix.go
@@ -9,7 +9,10 @@ import (
 )
 
 func killProcess(id int) error {
-	if err := unix.Kill(id, syscall.SIGKILL); err != nil && err != syscall.ESRCH {
+	// Ignore EPERM errors, as we may get those if we try to kill
+	// setuid processes. Also ignore ESRCH errors, as we can get
+	// those if we try to kill a process that already terminated.
+	if err := unix.Kill(id, syscall.SIGKILL); err != nil && err != syscall.EPERM && err != syscall.ESRCH {
 		return err
 	}
 	return nil
