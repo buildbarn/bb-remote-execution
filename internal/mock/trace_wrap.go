9f9ef219bf1dcdf95934f33774f4a1ce3262855f
Nils Wireklint <nils@meroton.com>
2023-12-13 14:16:06 +0100

    Upgrade all Go dependencies
    What's really annoying about the latest version of OpenTelemetry is that
    they've redefined the Span, Tracer and TracerProvider interfaces to
    contain private methods, so that you no longer have the ability to fully
    implement them yourself. This is done to provide gradual forward
    compatibility, but I don't buy it. In my case I want to get explicit
    build failures, so that I know I need to extend my implementations.
    
    Solve this by explicitly embedding embedded.* into our types. For gomock
    we now need to declare our very own wrappers. Very annoying.

---
+++ internal/mock/trace_wrap.go
@@ -0,0 +1,53 @@
+package mock
+
+import (
+	"github.com/golang/mock/gomock"
+
+	"go.opentelemetry.io/otel/trace/embedded"
+)
+
+// WrappedMockSpan is a wrapper around the gomock stub for trace.Span.
+// We need to add embedded.Span to it to satisfy the interface, as it
+// contains some private methods.
+type WrappedMockSpan struct {
+	embedded.Span
+	*BareMockSpan
+}
+
+// NewMockSpan creates a new gomock stub for trace.Span.
+func NewMockSpan(ctrl *gomock.Controller) *WrappedMockSpan {
+	return &WrappedMockSpan{
+		BareMockSpan: NewBareMockSpan(ctrl),
+	}
+}
+
+// WrappedMockTracer is a wrapper around the gomock stub for
+// trace.Tracer. We need to add embedded.Tracer to it to satisfy the
+// interface, as it contains some private methods.
+type WrappedMockTracer struct {
+	embedded.Tracer
+	*BareMockTracer
+}
+
+// NewMockTracer creates a new gomock stub for trace.Tracer.
+func NewMockTracer(ctrl *gomock.Controller) *WrappedMockTracer {
+	return &WrappedMockTracer{
+		BareMockTracer: NewBareMockTracer(ctrl),
+	}
+}
+
+// WrappedMockTracerProvider is a wrapper around the gomock stub for
+// trace.TracerProvider. We need to add embedded.TracerProvider to it to
+// satisfy the interface, as it contains some private methods.
+type WrappedMockTracerProvider struct {
+	embedded.TracerProvider
+	*BareMockTracerProvider
+}
+
+// NewMockTracerProvider creates a new gomock stub for
+// trace.TracerProvider.
+func NewMockTracerProvider(ctrl *gomock.Controller) *WrappedMockTracerProvider {
+	return &WrappedMockTracerProvider{
+		BareMockTracerProvider: NewBareMockTracerProvider(ctrl),
+	}
+}
