9f3fedf9d65d2f6f1c4c9ba43a223c2ba8b476d7
Ed Schouten <eschouten@apple.com>
2026-03-26 17:34:16 +0100

    Bump the deprecated API version to v2.3.0
    Because we always interpret Command.arguments[0] according to the rules
    of REv2.3, we shouldn't announce support for REv2.2 or lower, even as
    part of the deprecated API version.

--- pkg/scheduler/in_memory_build_queue.go
+++ pkg/scheduler/in_memory_build_queue.go
@@ -287,14 +287,7 @@ var inMemoryBuildQueueCapabilitiesProvider = capabilities.NewStaticProvider(&rem
 	// - REv2.1 changes the way output paths are declared in Command.
 	// - REv2.2 moves the platform properties from Command to Action.
 	// - REv2.3 changes the way Command.arguments[0] is interpreted.
-	//
-	// We should set the deprecated API version to REv2.3 as well,
-	// as we always process Commands.arguments[0] according to the
-	// new semantics. However, that would break compatibility with
-	// Bazel 8.x and below.
-	//
-	// TODO: Bump the deprecated API version to REv2.3 on 2028-01-01.
-	DeprecatedApiVersion: &semver.SemVer{Major: 2, Minor: 2},
+	DeprecatedApiVersion: &semver.SemVer{Major: 2, Minor: 3},
 	LowApiVersion:        &semver.SemVer{Major: 2, Minor: 3},
 	HighApiVersion:       &semver.SemVer{Major: 2, Minor: 12},
 })
