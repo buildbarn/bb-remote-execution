6f5cfd2d77b39895784e089fe82898b180a6d9e6
Mauricio Galindo <mgalindo@snapchat.com>
2026-09-10 17:16:59 -0700

    Extract helper logic from local executor so that it can be shared with the persistent executor

--- pkg/builder/local_build_executor.go
+++ pkg/builder/local_build_executor.go
@@ -2,12 +2,9 @@ package builder
 
 import (
 	"context"
-	"os"
-	"sync"
 	"time"
 
 	remoteexecution "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
-	re_clock "github.com/buildbarn/bb-remote-execution/pkg/clock"
 	"github.com/buildbarn/bb-remote-execution/pkg/filesystem/access"
 	"github.com/buildbarn/bb-remote-execution/pkg/filesystem/pool"
 	"github.com/buildbarn/bb-remote-execution/pkg/proto/remoteworker"
@@ -22,48 +19,8 @@ import (
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/types/known/durationpb"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
-
-// Filenames of objects to be created inside the build directory.
-var (
-	stdoutComponent              = path.MustNewComponent("stdout")
-	stderrComponent              = path.MustNewComponent("stderr")
-	deviceDirectoryComponent     = path.MustNewComponent("dev")
-	inputRootDirectoryComponent  = path.MustNewComponent("root")
-	serverLogsDirectoryComponent = path.MustNewComponent("server_logs")
-	temporaryDirectoryComponent  = path.MustNewComponent("tmp")
-	checkReadinessComponent      = path.MustNewComponent("check_readiness")
-)
-
-// capturingErrorLogger is an error logger that stores up to a single
-// error. When the error is stored, a context cancelation function is
-// invoked. This is used by localBuildExecutor to kill a build action in
-// case an I/O error occurs on the FUSE file system.
-type capturingErrorLogger struct {
-	lock   sync.Mutex
-	cancel context.CancelFunc
-	error  error
-}
-
-func (el *capturingErrorLogger) Log(err error) {
-	el.lock.Lock()
-	defer el.lock.Unlock()
-
-	if el.cancel != nil {
-		el.error = err
-		el.cancel()
-		el.cancel = nil
-	}
-}
-
-func (el *capturingErrorLogger) GetError() error {
-	el.lock.Lock()
-	defer el.lock.Unlock()
-
-	return el.error
-}
 
 type localBuildExecutor struct {
 	contentAddressableStorage      blobstore.BlobAccess
@@ -91,23 +48,6 @@ func NewLocalBuildExecutor(contentAddressableStorage blobstore.BlobAccess, comma
 		environmentVariables:           environmentVariables,
 		forceUploadTreesAndDirectories: forceUploadTreesAndDirectories,
 	}
-}
-
-func (be *localBuildExecutor) createCharacterDevices(inputRootDirectory BuildDirectory) error {
-	if err := inputRootDirectory.Mkdir(deviceDirectoryComponent, 0o777); err != nil && !os.IsExist(err) {
-		return util.StatusWrap(err, "Unable to create /dev directory in input root")
-	}
-	deviceDirectory, err := inputRootDirectory.EnterBuildDirectory(deviceDirectoryComponent)
-	if err != nil {
-		return util.StatusWrap(err, "Unable to enter /dev directory in input root")
-	}
-	defer deviceDirectory.Close()
-	for name, number := range be.inputRootCharacterDevices {
-		if err := deviceDirectory.Mknod(name, os.ModeDevice|os.ModeCharDevice|0o666, number); err != nil {
-			return util.StatusWrapf(err, "Failed to create character device %#v", name.String())
-		}
-	}
-	return nil
 }
 
 func (be *localBuildExecutor) CheckReadiness(ctx context.Context) error {
@@ -205,36 +145,16 @@ func (be *localBuildExecutor) Execute(ctx context.Context, filePool pool.FilePoo
 	}
 	defer inputRootDirectory.Close()
 
-	inputRootDigest, err := digestFunction.NewDigestFromProto(action.InputRootDigest)
-	if err != nil {
-		attachErrorToExecuteResponse(
-			response,
-			util.StatusWrap(err, "Failed to extract digest for input root"),
-		)
-		return response
-	}
-	if err := inputRootDirectory.MergeDirectoryContents(ctx, &ioErrorCapturer, inputRootDigest, monitor); err != nil {
+	if err := populateInputRoot(ctx, inputRootDirectory, &ioErrorCapturer, digestFunction, action.InputRootDigest, monitor, be.inputRootCharacterDevices); err != nil {
 		attachErrorToExecuteResponse(response, err)
 		return response
 	}
 
-	if len(be.inputRootCharacterDevices) > 0 {
-		if err := be.createCharacterDevices(inputRootDirectory); err != nil {
-			attachErrorToExecuteResponse(response, err)
-			return response
-		}
-	}
-
 	// Create parent directories of output files and directories.
 	// These are not declared in the input root explicitly.
-	commandDigest, err := digestFunction.NewDigestFromProto(action.CommandDigest)
+	command, err := readCommand(ctx, be.commandReader, digestFunction, action.CommandDigest)
 	if err != nil {
-		attachErrorToExecuteResponse(response, util.StatusWrap(err, "Failed to extract digest for command"))
-		return response
-	}
-	command, err := be.commandReader.ReadMessage(ctx, commandDigest)
-	if err != nil {
-		attachErrorToExecuteResponse(response, util.StatusWrap(err, "Failed to obtain command"))
+		attachErrorToExecuteResponse(response, err)
 		return response
 	}
 	outputHierarchy, err := NewOutputHierarchy(command)
@@ -275,13 +195,7 @@ func (be *localBuildExecutor) Execute(ctx context.Context, filePool pool.FilePoo
 		},
 	}
 
-	environmentVariables := map[string]string{}
-	for name, value := range be.environmentVariables {
-		environmentVariables[name] = value
-	}
-	for _, environmentVariable := range command.EnvironmentVariables {
-		environmentVariables[environmentVariable.Name] = environmentVariable.Value
-	}
+	environmentVariables := getCommandEnvironmentVariables(command, be.environmentVariables)
 
 	// Invoke the command.
 	ctxWithTimeout, cancelTimeout := be.clock.NewContextWithTimeout(ctxWithIOError, executionTimeout)
@@ -298,26 +212,7 @@ func (be *localBuildExecutor) Execute(ctx context.Context, filePool pool.FilePoo
 	cancelTimeout()
 	<-ctxWithTimeout.Done()
 
-	// If an I/O error occurred during execution, attach any errors
-	// related to it to the response first. These errors should be
-	// preferred over the cancelation errors that are a result of it.
-	if err := ioErrorCapturer.GetError(); err != nil {
-		attachErrorToExecuteResponse(response, util.StatusWrap(err, "I/O error while running command"))
-	}
-
-	// Attach the exit code or execution error.
-	if runErr == nil {
-		response.Result.ExitCode = int32(runResponse.ExitCode)
-		response.Result.ExecutionMetadata.AuxiliaryMetadata = append(response.Result.ExecutionMetadata.AuxiliaryMetadata, runResponse.ResourceUsage...)
-	} else {
-		attachErrorToExecuteResponse(response, util.StatusWrap(runErr, "Failed to run command"))
-	}
-
-	// For FUSE-based workers: Attach the amount of time the action
-	// ran, minus the time it was delayed reading data from storage.
-	if unsuspendedDuration, ok := ctxWithTimeout.Value(re_clock.UnsuspendedDurationKey{}).(time.Duration); ok {
-		response.Result.ExecutionMetadata.VirtualExecutionDuration = durationpb.New(unsuspendedDuration)
-	}
+	attachExecutionResult(ctxWithTimeout, response, int32(runResponse.GetExitCode()), runResponse.GetResourceUsage(), runErr, ioErrorCapturer.GetError())
 
 	executionStateUpdates <- &remoteworker.CurrentState_Executing{
 		ActionDigest: request.ActionDigest,
@@ -328,73 +223,7 @@ func (be *localBuildExecutor) Execute(ctx context.Context, filePool pool.FilePoo
 
 	writableFileUploadDelayCtx, writableFileUploadDelayCancel := be.clock.NewContextWithTimeout(ctx, be.maximumWritableFileUploadDelay)
 	defer writableFileUploadDelayCancel()
-	writableFileUploadDelayChan := writableFileUploadDelayCtx.Done()
-
-	// Upload command output. In the common case, the stdout and
-	// stderr files are empty. If that's the case, don't bother
-	// setting the digest to keep the ActionResult small.
-	if stdoutDigest, err := buildDirectory.UploadFile(ctx, stdoutComponent, digestFunction, writableFileUploadDelayChan); err != nil {
-		attachErrorToExecuteResponse(response, util.StatusWrap(err, "Failed to store stdout"))
-	} else if stdoutDigest.GetSizeBytes() > 0 {
-		response.Result.StdoutDigest = stdoutDigest.GetProto()
-	}
-	if stderrDigest, err := buildDirectory.UploadFile(ctx, stderrComponent, digestFunction, writableFileUploadDelayChan); err != nil {
-		attachErrorToExecuteResponse(response, util.StatusWrap(err, "Failed to store stderr"))
-	} else if stderrDigest.GetSizeBytes() > 0 {
-		response.Result.StderrDigest = stderrDigest.GetProto()
-	}
-	if err := outputHierarchy.UploadOutputs(ctx, inputRootDirectory, be.contentAddressableStorage, digestFunction, writableFileUploadDelayChan, response.Result, be.forceUploadTreesAndDirectories); err != nil {
-		attachErrorToExecuteResponse(response, err)
-	}
-
-	// Recursively traverse the server logs directory and attach any
-	// file stored within to the ExecuteResponse.
-	serverLogsDirectoryUploader := serverLogsDirectoryUploader{
-		context:                 ctx,
-		executeResponse:         response,
-		digestFunction:          digestFunction,
-		writableFileUploadDelay: writableFileUploadDelayChan,
-	}
-	serverLogsDirectoryUploader.uploadDirectory(buildDirectory, serverLogsDirectoryComponent, nil)
+	uploadBuildOutputs(ctx, buildDirectory, inputRootDirectory, outputHierarchy, be.contentAddressableStorage, digestFunction, writableFileUploadDelayCtx.Done(), response, be.forceUploadTreesAndDirectories)
 
 	return response
-}
-
-type serverLogsDirectoryUploader struct {
-	context                 context.Context
-	executeResponse         *remoteexecution.ExecuteResponse
-	digestFunction          digest.Function
-	writableFileUploadDelay <-chan struct{}
-}
-
-func (u *serverLogsDirectoryUploader) uploadDirectory(parentDirectory UploadableDirectory, dName path.Component, dPath *path.Trace) {
-	d, err := parentDirectory.EnterUploadableDirectory(dName)
-	if err != nil {
-		attachErrorToExecuteResponse(u.executeResponse, util.StatusWrapf(err, "Failed to enter server logs directory %#v", dPath.GetUNIXString()))
-		return
-	}
-	defer d.Close()
-
-	files, err := d.ReadDir()
-	if err != nil {
-		attachErrorToExecuteResponse(u.executeResponse, util.StatusWrapf(err, "Failed to read server logs directory %#v", dPath.GetUNIXString()))
-		return
-	}
-
-	for _, file := range files {
-		childName := file.Name()
-		childPath := dPath.Append(childName)
-		switch fileType := file.Type(); fileType {
-		case filesystem.FileTypeRegularFile:
-			if childDigest, err := d.UploadFile(u.context, childName, u.digestFunction, u.writableFileUploadDelay); err == nil {
-				u.executeResponse.ServerLogs[childPath.GetUNIXString()] = &remoteexecution.LogFile{
-					Digest: childDigest.GetProto(),
-				}
-			} else {
-				attachErrorToExecuteResponse(u.executeResponse, util.StatusWrapf(err, "Failed to store server log %#v", childPath.GetUNIXString()))
-			}
-		case filesystem.FileTypeDirectory:
-			u.uploadDirectory(d, childName, childPath)
-		}
-	}
 }
--- pkg/builder/local_build_executor_test.go
+++ pkg/builder/local_build_executor_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/buildbarn/bb-remote-execution/pkg/builder"
 	re_clock "github.com/buildbarn/bb-remote-execution/pkg/clock"
 	"github.com/buildbarn/bb-remote-execution/pkg/filesystem/access"
+	"github.com/buildbarn/bb-remote-execution/pkg/filesystem/pool"
 	"github.com/buildbarn/bb-remote-execution/pkg/proto/remoteworker"
 	runner_pb "github.com/buildbarn/bb-remote-execution/pkg/proto/runner"
 	"github.com/buildbarn/bb-storage/pkg/blobstore/buffer"
@@ -800,6 +801,158 @@ func TestLocalBuildExecutorSuccess(t *testing.T) {
 			},
 		},
 	}, executeResponse)
+}
+
+func TestLocalBuildExecutorResultHandling(test *testing.T) {
+	for _, testCase := range []struct {
+		name         string
+		runError     error
+		ioError      error
+		stdoutError  error
+		stderrError  error
+		closeError   error
+		expectedCode codes.Code
+	}{
+		{
+			name:         "NonzeroExitCodeStillUploadsOutputs",
+			expectedCode: codes.OK,
+		},
+		{
+			name:         "RunnerErrorBeforeUploadAndCloseErrors",
+			runError:     status.Error(codes.Unavailable, "runner unavailable"),
+			stdoutError:  status.Error(codes.ResourceExhausted, "stdout upload failed"),
+			closeError:   status.Error(codes.Internal, "close failed"),
+			expectedCode: codes.Unavailable,
+		},
+		{
+			name:         "IOErrorBeforeRunnerCancellation",
+			runError:     status.Error(codes.Canceled, "execution canceled"),
+			ioError:      status.Error(codes.DataLoss, "input read failed"),
+			stdoutError:  status.Error(codes.ResourceExhausted, "stdout upload failed"),
+			expectedCode: codes.DataLoss,
+		},
+		{
+			name:         "StdoutErrorBeforeStderrAndCloseErrors",
+			stdoutError:  status.Error(codes.ResourceExhausted, "stdout upload failed"),
+			stderrError:  status.Error(codes.PermissionDenied, "stderr upload failed"),
+			closeError:   status.Error(codes.Internal, "close failed"),
+			expectedCode: codes.ResourceExhausted,
+		},
+		{
+			name:         "StderrErrorStillUploadsOutputsAndLogs",
+			stderrError:  status.Error(codes.PermissionDenied, "stderr upload failed"),
+			expectedCode: codes.PermissionDenied,
+		},
+		{
+			name:         "CloseErrorAfterSuccessfulUpload",
+			closeError:   status.Error(codes.Internal, "close failed"),
+			expectedCode: codes.Internal,
+		},
+	} {
+		test.Run(testCase.name, func(test *testing.T) {
+			ctrl, ctx := gomock.WithContext(context.Background(), test)
+			digestFunction := digest.MustNewFunction("", remoteexecution.DigestFunction_SHA256)
+			emptyDigest := digest.MustNewDigest("", remoteexecution.DigestFunction_SHA256, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", 0)
+			actionDigest := digest.MustNewDigest("", remoteexecution.DigestFunction_SHA256, "0000000000000000000000000000000000000000000000000000000000000001", 123)
+			commandDigest := digest.MustNewDigest("", remoteexecution.DigestFunction_SHA256, "0000000000000000000000000000000000000000000000000000000000000002", 234)
+			inputRootDigest := digest.MustNewDigest("", remoteexecution.DigestFunction_SHA256, "0000000000000000000000000000000000000000000000000000000000000003", 345)
+			outputDigest := digest.MustNewDigest("", remoteexecution.DigestFunction_SHA256, "0000000000000000000000000000000000000000000000000000000000000004", 456)
+
+			contentAddressableStorage := mock.NewMockBlobAccess(ctrl)
+			commandReader := mock.NewMockMessageReader[*remoteexecution.Command](ctrl)
+			commandReader.EXPECT().ReadMessage(ctx, commandDigest).Return(&remoteexecution.Command{
+				Arguments:   []string{"compiler"},
+				OutputPaths: []string{"output"},
+			}, nil)
+			buildDirectory := mock.NewMockBuildDirectory(ctrl)
+			buildDirectoryCreator := mock.NewMockBuildDirectoryCreator(ctrl)
+			buildDirectoryCreator.EXPECT().GetBuildDirectory(ctx, &actionDigest).Return(buildDirectory, (*path.Trace)(nil), nil)
+			filePool := mock.NewMockFilePool(ctrl)
+			monitor := mock.NewMockUnreadDirectoryMonitor(ctrl)
+			var errorLogger util.ErrorLogger
+			buildDirectory.EXPECT().InstallHooks(filePool, gomock.Any()).Do(func(_ pool.FilePool, logger util.ErrorLogger) {
+				errorLogger = logger
+			})
+			buildDirectory.EXPECT().Mkdir(path.MustNewComponent("root"), os.FileMode(0o777))
+			inputRootDirectory := mock.NewMockBuildDirectory(ctrl)
+			buildDirectory.EXPECT().EnterBuildDirectory(path.MustNewComponent("root")).Return(inputRootDirectory, nil)
+			inputRootDirectory.EXPECT().MergeDirectoryContents(ctx, gomock.Any(), inputRootDigest, monitor)
+			buildDirectory.EXPECT().Mkdir(path.MustNewComponent("tmp"), os.FileMode(0o777))
+			buildDirectory.EXPECT().Mkdir(path.MustNewComponent("server_logs"), os.FileMode(0o777))
+
+			clock := mock.NewMockClock(ctrl)
+			clock.EXPECT().NewContextWithTimeout(gomock.Any(), time.Hour).DoAndReturn(func(parent context.Context, _ time.Duration) (context.Context, context.CancelFunc) {
+				return context.WithCancel(parent)
+			})
+			clock.EXPECT().NewContextWithTimeout(ctx, 10*time.Second).DoAndReturn(func(parent context.Context, _ time.Duration) (context.Context, context.CancelFunc) {
+				return context.WithCancel(parent)
+			})
+			runner := mock.NewMockRunnerClient(ctrl)
+			var executionContext context.Context
+			runCall := runner.EXPECT().Run(gomock.Any(), gomock.Any()).DoAndReturn(func(runContext context.Context, _ *runner_pb.RunRequest, _ ...grpc.CallOption) (*runner_pb.RunResponse, error) {
+				executionContext = runContext
+				if testCase.ioError != nil {
+					errorLogger.Log(testCase.ioError)
+					require.Error(test, runContext.Err())
+				}
+				if testCase.runError != nil {
+					return nil, testCase.runError
+				}
+				return &runner_pb.RunResponse{ExitCode: 17}, nil
+			})
+			serverLogsDirectory := mock.NewMockUploadableDirectory(ctrl)
+			gomock.InOrder(
+				runCall,
+				buildDirectory.EXPECT().UploadFile(ctx, path.MustNewComponent("stdout"), digestFunction, gomock.Any()).DoAndReturn(func(uploadContext context.Context, _ path.Component, _ digest.Function, _ <-chan struct{}) (digest.Digest, error) {
+					require.Error(test, executionContext.Err())
+					require.NoError(test, uploadContext.Err())
+					return emptyDigest, testCase.stdoutError
+				}),
+				buildDirectory.EXPECT().UploadFile(ctx, path.MustNewComponent("stderr"), digestFunction, gomock.Any()).Return(emptyDigest, testCase.stderrError),
+				inputRootDirectory.EXPECT().Lstat(path.MustNewComponent("output")).Return(filesystem.NewFileInfo(path.MustNewComponent("output"), filesystem.FileTypeRegularFile, false), nil),
+				inputRootDirectory.EXPECT().UploadFile(ctx, path.MustNewComponent("output"), digestFunction, gomock.Any()).Return(outputDigest, nil),
+				buildDirectory.EXPECT().EnterUploadableDirectory(path.MustNewComponent("server_logs")).Return(serverLogsDirectory, nil),
+				serverLogsDirectory.EXPECT().ReadDir().Return([]filesystem.FileInfo{filesystem.NewFileInfo(path.MustNewComponent("diagnostic"), filesystem.FileTypeRegularFile, false)}, nil),
+				serverLogsDirectory.EXPECT().UploadFile(ctx, path.MustNewComponent("diagnostic"), digestFunction, gomock.Any()).Return(outputDigest, nil),
+				serverLogsDirectory.EXPECT().Close(),
+				inputRootDirectory.EXPECT().Close(),
+				buildDirectory.EXPECT().Close().Return(testCase.closeError),
+			)
+
+			localBuildExecutor := builder.NewLocalBuildExecutor(contentAddressableStorage, commandReader, buildDirectoryCreator, runner, clock, 10*time.Second, nil, nil, false)
+			stateUpdates := make(chan *remoteworker.CurrentState_Executing, 3)
+			response := localBuildExecutor.Execute(ctx, filePool, monitor, digestFunction, &remoteworker.DesiredState_Executing{
+				ActionDigest: actionDigest.GetProto(),
+				Action: &remoteexecution.Action{
+					CommandDigest:   commandDigest.GetProto(),
+					InputRootDigest: inputRootDigest.GetProto(),
+					Timeout:         durationpb.New(time.Hour),
+				},
+			}, stateUpdates)
+
+			responseError := status.ErrorProto(response.Status)
+			if testCase.expectedCode == codes.OK {
+				require.NoError(test, responseError)
+			} else {
+				require.Error(test, responseError)
+				require.Equal(test, testCase.expectedCode, status.Code(responseError))
+			}
+			if testCase.runError == nil {
+				require.Equal(test, int32(17), response.Result.ExitCode)
+			} else {
+				require.Zero(test, response.Result.ExitCode)
+			}
+			require.Nil(test, response.Result.StdoutDigest)
+			require.Nil(test, response.Result.StderrDigest)
+			require.Len(test, response.Result.OutputFiles, 1)
+			testutil.RequireEqualProto(test, &remoteexecution.OutputFile{Path: "output", Digest: outputDigest.GetProto()}, response.Result.OutputFiles[0])
+			testutil.RequireEqualProto(test, &remoteexecution.LogFile{Digest: outputDigest.GetProto()}, response.ServerLogs["diagnostic"])
+			require.Len(test, stateUpdates, 3)
+			require.NotNil(test, (<-stateUpdates).GetFetchingInputs())
+			require.NotNil(test, (<-stateUpdates).GetRunning())
+			require.NotNil(test, (<-stateUpdates).GetUploadingOutputs())
+		})
+	}
 }
 
 func TestLocalBuildExecutorCachingInvalidTimeout(t *testing.T) {
